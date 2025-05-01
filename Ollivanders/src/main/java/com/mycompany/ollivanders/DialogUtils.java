8fe87cd5f71abc3566d4c32d815278133a411fdf
ChulpanKhan <chulpan.khannanova@gmail.com>
2025-05-01 23:52:25 +0300

    диалог

---
+++ Ollivanders/src/main/java/com/mycompany/ollivanders/DialogUtils.java
@@ -0,0 +1,25 @@
+
+package com.mycompany.ollivanders;
+
+import javax.swing.JOptionPane;
+
+public class DialogUtils {
+
+    public static void showSuccessDialog(String message) {
+        JOptionPane.showMessageDialog(
+                null,
+                message,
+                "Успех",
+                JOptionPane.INFORMATION_MESSAGE
+        );
+    }
+
+    public static void showErrorMessage(String message) {
+        JOptionPane.showMessageDialog(
+                null,
+                message,
+                "Ошибка",
+                JOptionPane.ERROR_MESSAGE
+        );
+    }
+}
